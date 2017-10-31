b5cc845b499c1c52871e113b28fb666b07126ed3
michaeltur3 <michaeltur3@gmail.com>
2017-10-31 18:31:21 +0200

    testing git

--- ex1/ParallelGameOfLife.java
+++ ex1/ParallelGameOfLife.java
@@ -5,6 +5,7 @@ public class ParallelGameOfLife implements GameOfLife {
 	public boolean[][][] invoke(boolean[][] initalField, int hSplit, int vSplit,
 			int generations) {
 		// TODO Auto-generated method stub
+		// testing git
 		return null;
 	}
 
