85a2dec24bd0831449e51f272bbc82025299f8d6
Nadav-Levintov <DSP155@Gmail.com>
2017-11-03 15:26:32 +0200

    added stop condition

--- ex1/InnerCell.java
+++ ex1/InnerCell.java
@@ -4,13 +4,17 @@ import java.util.Queue;
 
 public class InnerCell extends Cell {
 
-    public InnerCell(Integer row, Integer col, Boolean value, Queue<Cell> threadWorkQueue,Integer generationsToDo,
+    public InnerCell(Integer row, Integer col, Boolean value, Queue<Cell> threadWorkQueue, Integer generationsToDo,
                      boolean[][][] results) {
-        super(row, col, value, threadWorkQueue,generationsToDo,results);
+        super(row, col, value, threadWorkQueue, generationsToDo, results);
     }
 
     @Override
     public cellUpdateResult updateValue() {
+
+        if (this.gen == this.generationsToDo - 1) {
+            return cellUpdateResult.CELL_UPDATE_FAIL;
+        }
 
         int liveNeighbours = 0;
         for (Cell neighbour :
@@ -18,7 +22,7 @@ public class InnerCell extends Cell {
             Integer neighbourGen = neighbour.getGen();
             int neighbourValue = 0;
             /*check that neighbor is not DeadCell and that is in relevant gen */
-            if (neighbourGen > -1 && (neighbourGen < this.gen - 1 || neighbourGen > this.gen))
+            if (neighbourGen > -1 && !(neighbourGen == this.gen || neighbourGen == this.gen + 1))
                 return cellUpdateResult.CELL_UPDATE_FAIL;
 
             if (neighbourGen.intValue() == this.gen) {
@@ -43,13 +47,11 @@ public class InnerCell extends Cell {
 
         threadWorkQueue.addAll(this.getNeighbors());
 
-        if(gen == generationsToDo-1 )
-        {
-            results[0][row][col]=value;
+        if (gen == generationsToDo - 1) {
+            results[0][row][col] = value;
         }
-        if(gen == generationsToDo )
-        {
-            results[1][row][col]=value;
+        if (gen == generationsToDo) {
+            results[1][row][col] = value;
         }
         return cellUpdateResult.CELL_UPDATE_SUCCESS;
     }
