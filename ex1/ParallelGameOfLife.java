7aeb9e6216047278f55664875cb7b70851ee1d85
Nadav-Levintov <DSP155@Gmail.com>
2017-11-04 11:32:44 +0200

    All works now. Need deeper testing but seems OK

--- ex1/ParallelGameOfLife.java
+++ ex1/ParallelGameOfLife.java
@@ -2,33 +2,33 @@ package ex1;
 
 public class ParallelGameOfLife implements GameOfLife {
 
-    public boolean[][][] invoke(boolean[][] initalField, int hSplit, int vSplit,
+    public boolean[][][] invoke(boolean[][] initialField, int hSplit, int vSplit,
                                 int generations) {
-        GameOfLifeThread[][] threadMatrix = new GameOfLifeThread[hSplit][vSplit];
-        ExternalCellQueue[][] queuesMatrix = new ExternalCellQueue[hSplit][vSplit];
-        boolean[][][] results = new boolean[2][initalField.length][initalField[0].length];
+        GameOfLifeThread[][] threadMatrix = new GameOfLifeThread[vSplit][hSplit];
+        ExternalCellQueue[][] queuesMatrix = new ExternalCellQueue[vSplit][hSplit];
+        boolean[][][] results = new boolean[2][initialField.length][initialField[0].length];
 
         /* Init all queues for all threads */
         int id = 0;
-        for (int i = 0; i < hSplit; i++) {
-            for (int j = 0; j < vSplit; j++) {
+        for (int i = 0; i < vSplit; i++) {
+            for (int j = 0; j < hSplit; j++) {
                 queuesMatrix[i][j] = new ExternalCellQueue(id);
                 id++;
             }
         }
 
         /* Init threads */
-        int rowStep = initalField.length / vSplit;
-        int colStep = initalField[0].length / hSplit;
+        int rowStep = initialField.length / vSplit;
+        int colStep = initialField[0].length / hSplit;
 
         for (int row = 0; row < vSplit; row++) {
             for (int col = 0; col < hSplit; col++) {
-                int height = (row == vSplit - 1) ? rowStep + (initalField.length % vSplit) : rowStep;
-                int width = (col == hSplit - 1) ? colStep + (initalField[0].length % hSplit) : colStep;
+                int height = (row == vSplit - 1) ? rowStep + (initialField.length % vSplit) : rowStep;
+                int width = (col == hSplit - 1) ? colStep + (initialField[0].length % hSplit) : colStep;
                 ExternalCellQueue[][] currThreadQueueMatrix = buildCellQueueMatrix(row, col, queuesMatrix);
 
                 threadMatrix[row][col] = new GameOfLifeThread(height, width, row * rowStep,
-                        col * colStep, currThreadQueueMatrix, generations, initalField, results);
+                        col * colStep, currThreadQueueMatrix, generations, initialField, results);
                 threadMatrix[row][col].start();
             }
         }
@@ -52,12 +52,14 @@ public class ParallelGameOfLife implements GameOfLife {
         ExternalCellQueue[][] newMatrix = new ExternalCellQueue[matrixSize][matrixSize];
 
         for (int row = 0; row < matrixSize; row++) {
+            if ((currRow == 0 && row == 0) || (currRow >= allQueuesMatrix.length - 1 && row >= matrixSize - 1))
+                continue;
             for (int col = 0; col < matrixSize; col++) {
-                if ((currRow == 0 && row == 0) || (currRow == allQueuesMatrix.length - 1 && row == matrixSize - 1))
+                if ((currCol == 0 && col == 0) || (currCol >= allQueuesMatrix[0].length - 1 && col >= matrixSize - 1))
                     continue;
-                if ((currCol == 0 && col == 0) || (currCol == allQueuesMatrix[0].length - 1 && col == matrixSize - 1))
-                    continue;
-                newMatrix[row][col] = allQueuesMatrix[currRow + row - 1][currCol + col - 1];
+                int rowInBigMatrix = currRow + row - 1;
+                int colInBigMatrix = currCol + col - 1;
+                newMatrix[row][col] = allQueuesMatrix[rowInBigMatrix][colInBigMatrix];
             }
         }
 
